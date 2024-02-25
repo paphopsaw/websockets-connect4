86f723748af1da6395b151d1a7dd759362a246c1
Paphop Sawasdee <paphop.staw@gmail.com>
2024-02-25 17:32:13 +0900

    Add message from websocket server

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
   createBoard(board);
 
   const websocket = new WebSocket("ws://localhost:8001/");
+  receiveMoves(board, websocket);
   sendMoves(board, websocket);
 });
 
@@ -22,4 +23,28 @@ function sendMoves(board, websocket) {
     };
     websocket.send(JSON.stringify(event));
   })
+}
+
+function showMessage(message) {
+  window.setTimeout(() => window.alert(message), 50);
+}
+
+function receiveMoves(board, websocket) {
+  websocket.addEventListener("message", ({ data }) => {
+    const event = JSON.parse(data);
+    switch (event.type) {
+      case "play":
+        playMove(board, event.player, event.column, event.row);
+        break;
+      case "win":
+        showMessage(`Player ${event.player} wins!`);
+        websocket.close(1000);
+        break;
+      case "error":
+        showMessage(event.message);
+        break;
+      default:
+        throw new Error(`Unsupported event of type ${event.type}.`);
+    }
+  });
 }
